51d77121481a5bd76adfb051ba92da05db0190da
heriberto <sosaheriberto2001@gmail.com>
2025-02-05 09:46:51 -0400

    Primer commit: Código inicial del módulo

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "curso 1 - Motorcycle Register",
+    'summary': "Manage mortorcycle registers",
+    'description': """Manage mortorcycle registers""",
+    'license': 'OPL-1',
+    'author': "Heriberto Sosa",
+    'website': "https://www.humanssoftware.site",
+    'category': 'Custom Modules/HS',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml'
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'application': True,
+}
+
